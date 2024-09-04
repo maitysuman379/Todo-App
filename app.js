03a2833a79578ff2c3ff154cf85d35f73625cdd6
maitysuman379 <maitysuman379@gmail.com>
2024-09-04 22:17:07 +0530

    complete

--- app.js
+++ app.js
@@ -5,6 +5,27 @@ let inp = document.querySelector('input');
 btn.addEventListener("click",function(){
     let item = document.createElement("li");
     item.innerText = inp.value;
+
+    let delbtn = document.createElement('button');
+    delbtn.innerText = "delete";
+    delbtn.classList.add("delete");
+
+    item.appendChild(delbtn);
     ul.appendChild(item);
     inp.value = "";
 });
+
+ul.addEventListener('click',function(event){
+    if(event.target.nodeName == "BUTTON"){
+        let listItem = event.target.parentElement;
+        listItem.remove();
+    }
+});
+
+// let delbtns = document.querySelectorAll('.delete');
+// for(delbtn of delbtns){
+//     delbtn.addEventListener('click',function(){
+//         let par = this.parentElement;
+//         par.remove();
+//     });
+// }
